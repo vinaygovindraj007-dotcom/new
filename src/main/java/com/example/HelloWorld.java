e451dd15d99f27827e4f09049f8f620c2a971d3f
Monkey_d_luffy <vinaygovindraj007@gmail.com>
2025-11-19 19:10:37 +0530

    Create HelloWorld.java

---
+++ src/main/java/com/example/HelloWorld.java
@@ -0,0 +1,7 @@
+package com.example;
+public class HelloWorld {
+public static void main(String[] args) {
+System.out.println(&quot;Hello, DevOps CI/CD using GitHub Actions!&quot;);
+System.out.println(&quot;Change #1: Verifying automatic workflow trigger on push.&quot;);
+}
+}
